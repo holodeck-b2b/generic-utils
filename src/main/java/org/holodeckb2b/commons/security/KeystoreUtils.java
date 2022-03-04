9ef97ecc85ee737773e971f894b04bbff1bc22a8
Sander Fieten <sander@holodeck-b2b.org>
2022-03-04 14:30:52 +0100

    Add methods to read and save PKCS12 from/to stream

--- src/main/java/org/holodeckb2b/commons/security/KeystoreUtils.java
+++ src/main/java/org/holodeckb2b/commons/security/KeystoreUtils.java
@@ -1,24 +1,27 @@
 /*******************************************************************************
  * Copyright (C) 2020 The Holodeck Team, Sander Fieten
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package org.holodeckb2b.commons.security;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.KeyStore;
@@ -27,9 +30,9 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.Enumeration;
-
 import org.holodeckb2b.commons.util.Utils;
 
 /**
@@ -49,30 +52,74 @@ public final class KeystoreUtils {
 	/**
 	 * Reads the key pair from the specified PKCS#12 file. The PKCS#12 file must contain only one key pair that uses the
 	 * same password as for the file itself for this method to work.
-	 * 
+	 *
 	 * @param pkcs12File     Path to the PKCS#12 file
 	 * @param pkcs12Password Password to get access to the PKCS#12 file
-	 * @return The keypair read from the PKCS#12 file
-	 * @throws CertificateException When the keypair cannot be read from the specified file, for example because the
+	 * @return The key pair read from the PKCS#12 file
+	 * @throws CertificateException When the key pair cannot be read from the specified file, for example because the
 	 *                              file is missing, an incorrect password is given or multiple entries exists in the
 	 *                              given file
 	 */
 	public static PrivateKeyEntry readKeyPairFromPKCS12(final Path pkcs12File, final String pkcs12Password)
 			throws CertificateException {
+		try (FileInputStream fis = new FileInputStream(pkcs12File.toFile())) {
+			return readKeyPairFromKeystore(fis, pkcs12Password);
+		} catch (IOException ex) {
+			throw new CertificateException("Cannot load PKCS#12 file [" + pkcs12File + "]!", ex);
+		}
+	}
+
+	/**
+	 * Reads the key pair from the keystore contained in the given input stream. The keystore must contain only one key
+	 * pair that uses the same password as for the keystore itself for this method to work.
+	 *
+	 * @param is		input stream containing the keystore
+	 * @param password 	password to get access to the keystore and key pair
+	 * @return The key pair read from the keystore
+	 * @throws CertificateException When the key pair cannot be read from the specified stream, for example because an
+	 * 								incorrect password is given or multiple entries exists in the keystore
+	 * @since 1.1.0
+	 */
+	public static PrivateKeyEntry readKeyPairFromKeystore(final InputStream is, final String password)
+																						throws CertificateException {
 		try {
-			final KeyStore keyStore = load(KeystoreType.PKCS12, pkcs12File, pkcs12Password);
+			final KeyStore keyStore = load(is, password);
 			final String kpAlias = getSingleKPAlias(keyStore);
-			final char[] pwd = (!Utils.isNullOrEmpty(pkcs12Password) ? pkcs12Password.toCharArray() : new char[] {});
+			final char[] pwd = (!Utils.isNullOrEmpty(password) ? password.toCharArray() : new char[] {});
 			return (PrivateKeyEntry) keyStore.getEntry(kpAlias, new KeyStore.PasswordProtection(pwd));
 		} catch (KeyStoreException | UnrecoverableEntryException | NoSuchAlgorithmException ex) {
-			throw new CertificateException("Cannot load keypair from specified PKCS#12 file [" + pkcs12File + "]!", ex);
+			throw new CertificateException("Cannot load key pair from specified key store", ex);
+		}
+	}
+
+	/**
+	 * Writes the key pair in PKCS#12 format to the given output stream.
+	 *
+	 * @param pk		key pair to be written to the stream
+	 * @param os		output stream to write the key pair to
+	 * @param password 	password to secure the keystore and key pair
+	 * @throws CertificateException When the key pair cannot be read from the specified stream, for example because an
+	 * 								incorrect password is given or multiple entries exists in the keystore
+	 * @since 1.1.0
+	 */
+	public static void saveKeyPairToPKCS12(final PrivateKeyEntry pk, final OutputStream os, final String password)
+																						throws CertificateException {
+		try {
+			final KeyStore keyStore = KeyStore.getInstance(KeystoreType.PKCS12.name());
+			keyStore.load(null, null);
+			final char[] pwd = (!Utils.isNullOrEmpty(password) ? password.toCharArray() : new char[] {});
+			final String alias = CertificateUtils.getSubjectCN((X509Certificate) pk.getCertificate());
+			keyStore.setEntry(!Utils.isNullOrEmpty(alias) ? alias : "1", pk, new KeyStore.PasswordProtection(pwd));
+			keyStore.store(os, pwd);
+		} catch (KeyStoreException  | IOException | NoSuchAlgorithmException | CertificateException ex) {
+			throw new CertificateException("Could not write the key pair to output stream", ex);
 		}
 	}
 
 	/**
 	 * Checks that the specified PKCS#12 file contains exactly one key pair that is accessible using the given password
 	 * and returns the alias of that key pair.
-	 * 
+	 *
 	 * @param pkcs12File     Path to the PKCS#12 file
 	 * @param pkcs12Password Password to get access to the PKCS#12 file and key pair
 	 * @return The alias of the key pair stored in the PKCS#12 file
@@ -86,13 +133,13 @@ public final class KeystoreUtils {
 			final KeyStore keyStore = load(KeystoreType.PKCS12, pkcs12File, pkcs12Password);
 			return getSingleKPAlias(keyStore);
 		} catch (KeyStoreException ex) {
-			throw new CertificateException("Cannot load keypair from specified PKCS#12 file [" + pkcs12File + "]!", ex);
+			throw new CertificateException("Cannot load key pair from specified PKCS#12 file [" + pkcs12File + "]!", ex);
 		}
 	}
 
 	/**
 	 * Helper method to retrieve the alias of the single key pair contained in the keystore.
-	 * 
+	 *
 	 * @param ks keystore
 	 * @return the alias of the key pair
 	 * @throws KeyStoreException if there is a problem reading the entries in the keystore or when the keystore contains
@@ -103,7 +150,7 @@ public final class KeystoreUtils {
 		for (Enumeration<String> aliases = ks.aliases(); aliases.hasMoreElements();) {
 			final String a = aliases.nextElement();
 			if (ks.isKeyEntry(a) && kpAlias != null)
-				throw new KeyStoreException("More than one keypair in file!");
+				throw new KeyStoreException("More than one key pair in file!");
 			else if (ks.isKeyEntry(a))
 				kpAlias = a;
 		}
@@ -112,7 +159,7 @@ public final class KeystoreUtils {
 
 	/**
 	 * Checks whether the specified keystore is available and can be loaded. The specified file must be either a JKS
-	 * and/or PKCS#12 file. 
+	 * and/or PKCS#12 file.
 	 *
 	 * @param path Path to the keystore
 	 * @param pwd  Password to access the keystore
@@ -128,24 +175,26 @@ public final class KeystoreUtils {
 			return false;
 		}
 	}
-	
+
 	/**
-	 * Checks whether the specified keystore is available and can be loaded. 
+	 * Checks whether the specified keystore is available and can be loaded.
 	 *
 	 * @param path Path to the keystore
 	 * @param pwd  Password to access the keystore
-	 * @param type The expected type of the keustore, either JKS or PKCS#12 
+	 * @param type The expected type of the keustore, either JKS or PKCS#12
 	 */
 	public static boolean check(final Path path, final String pwd, final KeystoreType type) {
 		if (!Files.isRegularFile(path) || !Files.isReadable(path))
 			return false;
-				
-		try {
-			KeystoreType fType = detectStoreType(path);
+
+		try (BufferedInputStream fis = new BufferedInputStream(new FileInputStream(path.toFile()))) {
+			fis.mark(8);
+			KeystoreType fType = detectStoreType(fis);
 			if (fType != type)
 				return false;
-			
-			load(type, path, pwd);
+
+			fis.reset();
+			load(type, fis, pwd);
 			return true;
 		} catch (KeyStoreException | IOException spe) {
 			return false;
@@ -156,37 +205,57 @@ public final class KeystoreUtils {
 	private static final byte[] JCEKS_MAGIC_NO = new byte[] { (byte) 0xCE, (byte) 0xCE, (byte) 0xCE, (byte) 0xCE };
 
 	/**
-	 * Detects whether the keystore is in JKS, JCEKS or PKCS#12 format. This is checked by reading the first four bytes 
+	 * Detects whether the keystore is in JKS, JCEKS or PKCS#12 format. This is checked by reading the first four bytes
 	 * of the file and comparing this to the magic number that is at the start of JKS or JCEKS files (0xFEEDFEED resp.
 	 * 0xCECECECE).
-	 * 
+	 *
 	 * @param ksPath path to the keystore
 	 * @return the detected type
 	 * @throws IOException when the file at given path could not be read
+	 * @since 1.1.0
 	 */
-	private static KeystoreType detectStoreType(Path ksPath) throws IOException {
-		try (FileInputStream fis = new FileInputStream(ksPath.toFile())) {
-			byte[] magicno = new byte[4];
-			fis.read(magicno);
+	private static KeystoreType detectStoreType(InputStream is) throws IOException  {
+		byte[] magicno = new byte[4];
+		is.read(magicno);
 
-			return Arrays.equals(JKS_MAGIC_NO, magicno) ? KeystoreType.JKS :
-						Arrays.equals(JCEKS_MAGIC_NO, magicno) ? KeystoreType.JCEKS : KeystoreType.PKCS12;
-		}
+		return Arrays.equals(JKS_MAGIC_NO, magicno) ? KeystoreType.JKS :
+					Arrays.equals(JCEKS_MAGIC_NO, magicno) ? KeystoreType.JCEKS : KeystoreType.PKCS12;
 	}
 
 	/**
-	 * Loads the specified JKS, JCEKS or PKCS#12 keystore from disk.  
-	 * 
+	 * Loads the specified JKS, JCEKS or PKCS#12 keystore from disk.
+	 *
 	 * @param path 		Path to the keystore
 	 * @param password  Password to access the keystore
 	 * @return The keystore loaded from the specified file
 	 * @throws KeyStoreException When the keystore could not be loaded from the specified location.
 	 */
 	public static KeyStore load(final Path path, final String password) throws KeyStoreException {
-		try {
-			return load(detectStoreType(path), path, password);
+		try (FileInputStream fis = new FileInputStream(path.toFile())) {
+			return load(fis, password);
 		} catch (IOException ex) {
 			throw new KeyStoreException("Can not load the keystore [" + path + "]!", ex);
+		}
+	}
+
+	/**
+	 * Loads the specified JKS, JCEKS or PKCS#12 keystore from an input stream.
+	 *
+	 * @param is 		input stream containing the keystore
+	 * @param password  Password to access the keystore
+	 * @return The keystore loaded from the specified file
+	 * @throws KeyStoreException When the keystore could not be loaded from the specified location.
+	 * @since 1.1.0
+	 */
+	public static KeyStore load(final InputStream is, final String password) throws KeyStoreException {
+		InputStream mis = is.markSupported() ? is : new BufferedInputStream(is);
+		try {
+			mis.mark(8);
+			final KeystoreType type = detectStoreType(mis);
+			mis.reset();
+			return load(type, mis, password);
+		} catch (IOException loadError) {
+			throw new KeyStoreException("Can not load the keystore from input stream!", loadError);
 		}
 	}
 
@@ -200,15 +269,32 @@ public final class KeystoreUtils {
 	 * @throws KeyStoreException When the keystore could not be loaded from the specified location.
 	 */
 	public static KeyStore load(final KeystoreType type, final Path path, final String pwd) throws KeyStoreException {
+		try (FileInputStream fis = new FileInputStream(path.toFile())) {
+			return load(type, fis, pwd);
+		} catch (IOException ex) {
+			throw new KeyStoreException("Can not load the keystore [" + path + "]!", ex);
+		}
+	}
+
+	/**
+	 * Loads the specified keystore from an input stream
+	 *
+	 * @param type Indicates the keystore type, which can be either JKS or PKCS#12
+	 * @param is   input stream containing the keystore
+	 * @param pwd  Password to access the keystore
+	 * @return The keystore loaded from the specified file
+	 * @throws KeyStoreException When the keystore could not be loaded from the specified location.
+	 * @since 1.1.0
+	 */
+	public static KeyStore load(final KeystoreType type, final InputStream is, final String pwd)
+																							throws KeyStoreException {
 		try {
 			final KeyStore keyStore = KeyStore.getInstance(type.name());
-			try (FileInputStream fis = new java.io.FileInputStream(path.toFile())) {
-				keyStore.load(fis, (!Utils.isNullOrEmpty(pwd) ? pwd.toCharArray() : new char[] {}));
-			}
+			keyStore.load(is, (!Utils.isNullOrEmpty(pwd) ? pwd.toCharArray() : new char[] {}));
 			return keyStore;
 		} catch (NullPointerException | IOException | KeyStoreException | NoSuchAlgorithmException
 				| CertificateException ex) {
-			throw new KeyStoreException("Can not load the keystore [" + path + "]!", ex);
+			throw new KeyStoreException("Can not load the keystore from input stream!", ex);
 		}
 	}
 
--- src/test/java/org/holodeckb2b/commons/security/KeystoreUtilsTest.java
+++ src/test/java/org/holodeckb2b/commons/security/KeystoreUtilsTest.java
@@ -1,13 +1,8 @@
 package org.holodeckb2b.commons.security;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,48 +13,55 @@ import java.security.Security;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.util.Enumeration;
-
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.holodeckb2b.commons.security.KeystoreUtils.KeystoreType;
 import org.holodeckb2b.commons.testing.TestUtils;
 import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class KeystoreUtilsTest {
-	
+
 	@BeforeAll
 	static void setBCAsPrfdProvider() {
-		Security.insertProviderAt(new BouncyCastleProvider(), 1);		
+		Security.insertProviderAt(new BouncyCastleProvider(), 1);
 	}
-	
+
 	@AfterAll
 	static void removeBC() {
 		Security.removeProvider("BC");
 	}
 
 	@Test
-	void testGetPKFromPKCS12() {		
+	void testGetPKFromPKCS12() {
 		try {
 			assertNotNull(KeystoreUtils.readKeyPairFromPKCS12(TestUtils.getTestResource("singlekey.p12"), "singlekey"));
 		} catch (CertificateException e) {
 			fail(e);
 		}
-		
+
 		try {
-			assertNotNull(KeystoreUtils.readKeyPairFromPKCS12(TestUtils.getTestResource("keyandcert.p12"), 
+			assertNotNull(KeystoreUtils.readKeyPairFromPKCS12(TestUtils.getTestResource("keyandcert.p12"),
 																"keyandcert"));
 		} catch (CertificateException e) {
 			fail(e);
 		}
-		
+
 		PrivateKeyEntry pk = null;
 		try {
 			pk = KeystoreUtils.readKeyPairFromPKCS12(TestUtils.getTestResource("twokeys.p12"), "twokeys");
 			fail("Should have raised exception because of the two key pairs");
 		} catch (CertificateException e) {
 			assertNull(pk);
-		}		
+		}
 	}
 
 	@Test
@@ -75,28 +77,28 @@ class KeystoreUtilsTest {
 		} catch (Throwable t) {
 			fail(t);
 		}
-		
+
 		try {
-			assertTrue(KeystoreUtils.check(TestUtils.getTestResource("singlekey.p12"), "singlekey", 
+			assertTrue(KeystoreUtils.check(TestUtils.getTestResource("singlekey.p12"), "singlekey",
 											KeystoreType.PKCS12));
 		} catch (Throwable t) {
 			fail(t);
 		}
 		try {
-			assertFalse(KeystoreUtils.check(TestUtils.getTestResource("singlekey.p12"), "singlekey", 
+			assertFalse(KeystoreUtils.check(TestUtils.getTestResource("singlekey.p12"), "singlekey",
 											KeystoreType.JKS));
 		} catch (Throwable t) {
 			fail(t);
 		}
-		
+
 		try {
-			assertTrue(KeystoreUtils.check(TestUtils.getTestResource("trustedcerts.jks"), "trusted", 
+			assertTrue(KeystoreUtils.check(TestUtils.getTestResource("trustedcerts.jks"), "trusted",
 											KeystoreType.JKS));
 		} catch (Throwable t) {
 			fail(t);
 		}
 		try {
-			assertFalse(KeystoreUtils.check(TestUtils.getTestResource("trustedcerts.jks"), "trusted", 
+			assertFalse(KeystoreUtils.check(TestUtils.getTestResource("trustedcerts.jks"), "trusted",
 											KeystoreType.PKCS12));
 		} catch (Throwable t) {
 			fail(t);
@@ -107,25 +109,25 @@ class KeystoreUtilsTest {
 		} catch (Throwable t) {
 			fail(t);
 		}
-		
+
 		try {
-			assertTrue(KeystoreUtils.check(TestUtils.getTestResource("sharedkey.jceks"), "sharedkey", 
+			assertTrue(KeystoreUtils.check(TestUtils.getTestResource("sharedkey.jceks"), "sharedkey",
 											KeystoreType.JCEKS));
 		} catch (Throwable t) {
 			fail(t);
 		}
 		try {
-			assertFalse(KeystoreUtils.check(TestUtils.getTestResource("sharedkey.jceks"), "sharedkey", 
+			assertFalse(KeystoreUtils.check(TestUtils.getTestResource("sharedkey.jceks"), "sharedkey",
 											KeystoreType.JKS));
 		} catch (Throwable t) {
 			fail(t);
 		}
 	}
-	
+
 	@Test
 	void testLoadPKCS12() {
 		KeyStore ks = null;
-		
+
 		try {
 			ks = KeystoreUtils.load(TestUtils.getTestResource("keyandcert.p12"), "keyandcert");
 		} catch (Throwable t) {
@@ -153,9 +155,9 @@ class KeystoreUtilsTest {
 		} else {
 			assertEquals(2, n);
 			assertEquals(1, pk);
-			assertEquals(1, certs);			
+			assertEquals(1, certs);
 		}
-			
+
 		PrivateKeyEntry kp = null;
 		try {
 			kp = (PrivateKeyEntry) ks.getEntry("partyc", new KeyStore.PasswordProtection("keyandcert".toCharArray()));
@@ -167,11 +169,38 @@ class KeystoreUtilsTest {
 		assertNotNull(chain);
 		assertEquals(2, chain.length);
 	}
-	
+
+	@Test
+	void testSaveKeyPairAsPKCS12() {
+		PrivateKeyEntry kp = assertDoesNotThrow(() -> {
+			KeyStore ks = KeyStore.getInstance("PKCS12");
+			FileInputStream fis = new FileInputStream(TestUtils.getTestResource("singlekey.p12").toFile());
+			ks.load(fis, "singlekey".toCharArray());
+			fis.close();
+			return (PrivateKeyEntry) ks.getEntry(ks.aliases().nextElement(),
+													new KeyStore.PasswordProtection("singlekey".toCharArray()));
+		});
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+		assertDoesNotThrow(() -> KeystoreUtils.saveKeyPairToPKCS12(kp, baos, "savedkey"));
+
+		PrivateKeyEntry savedkp = assertDoesNotThrow(() -> {
+			KeyStore ks = KeyStore.getInstance("PKCS12");
+			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+			ks.load(bais, "savedkey".toCharArray());
+			return (PrivateKeyEntry) ks.getEntry(ks.aliases().nextElement(),
+													new KeyStore.PasswordProtection("savedkey".toCharArray()));
+		});
+
+		assertEquals(kp.getCertificate(), savedkp.getCertificate());
+		assertEquals(kp.getPrivateKey(), savedkp.getPrivateKey());
+	}
+
 	@Test
 	void testLoadJKS() {
 		KeyStore ks = null;
-		
+
 		try {
 			ks = KeystoreUtils.load(TestUtils.getTestResource("keyandcert.jks"), "keyandcert");
 		} catch (Throwable t) {
@@ -192,8 +221,8 @@ class KeystoreUtilsTest {
 		}
 		assertEquals(2, n);
 		assertEquals(1, pk);
-		assertEquals(1, certs);			
-		
+		assertEquals(1, certs);
+
 		PrivateKeyEntry kp = null;
 		try {
 			kp = (PrivateKeyEntry) ks.getEntry("partya", new KeyStore.PasswordProtection("keypair".toCharArray()));
@@ -205,17 +234,17 @@ class KeystoreUtilsTest {
 		assertNotNull(chain);
 		assertEquals(1, chain.length);
 	}
-	
+
 	@Test
 	void testSaveJKS() {
 		PrivateKeyEntry kp = null;
 		try {
 			KeyStore ks = KeystoreUtils.load(TestUtils.getTestResource("twokeys.p12"), "twokeys");
-			kp = (PrivateKeyEntry) ks.getEntry("partyb_dev",  new KeyStore.PasswordProtection("twokeys".toCharArray()));			
+			kp = (PrivateKeyEntry) ks.getEntry("partyb_dev",  new KeyStore.PasswordProtection("twokeys".toCharArray()));
 		} catch (Throwable t) {
 			fail(t);
 		}
-				
+
 		KeyStore ks = null;
 		try {
 			ks = KeyStore.getInstance(KeystoreType.JKS.name());
@@ -224,7 +253,7 @@ class KeystoreUtilsTest {
 		} catch (Exception e) {
 			fail(e);
 		}
-		
+
 		Path ksPath = TestUtils.getTestClassBasePath().resolve("newkeystore.jks");
 		try {
 			try {
@@ -232,12 +261,12 @@ class KeystoreUtilsTest {
 			} catch (Throwable t) {
 				fail(t);
 			}
-			
+
 			try {
 				ks = KeystoreUtils.load(KeystoreType.JKS, ksPath, "newkeystore");
 				assertTrue(ks.containsAlias("newentry"));
 				assertTrue(ks.isKeyEntry("newentry"));
-				PrivateKeyEntry copyKP = (PrivateKeyEntry) ks.getEntry("newentry", 
+				PrivateKeyEntry copyKP = (PrivateKeyEntry) ks.getEntry("newentry",
 																new KeyStore.PasswordProtection("newentry".toCharArray()));
 				assertArrayEquals(kp.getCertificate().getEncoded(), copyKP.getCertificate().getEncoded());
 			} catch (Throwable t) {
@@ -249,19 +278,19 @@ class KeystoreUtilsTest {
 			} catch (IOException e) {
 				System.err.println("Could not delete temp test file " + ksPath.toString());
 			}
-		}		
+		}
 	}
-	
+
 	@Test
 	void testSavePKCS12() {
 		PrivateKeyEntry kp = null;
 		try {
 			KeyStore ks = KeystoreUtils.load(TestUtils.getTestResource("twokeys.p12"), "twokeys");
-			kp = (PrivateKeyEntry) ks.getEntry("partyb_dev",  new KeyStore.PasswordProtection("twokeys".toCharArray()));			
+			kp = (PrivateKeyEntry) ks.getEntry("partyb_dev",  new KeyStore.PasswordProtection("twokeys".toCharArray()));
 		} catch (Throwable t) {
 			fail(t);
 		}
-		
+
 		KeyStore ks = null;
 		try {
 			ks = KeyStore.getInstance(KeystoreType.PKCS12.name());
@@ -270,7 +299,7 @@ class KeystoreUtilsTest {
 		} catch (Exception e) {
 			fail(e);
 		}
-		
+
 		Path ksPath = TestUtils.getTestClassBasePath().resolve("newkeystore.pfx");
 		try {
 			try {
@@ -278,12 +307,12 @@ class KeystoreUtilsTest {
 			} catch (Throwable t) {
 				fail(t);
 			}
-			
+
 			try {
 				ks = KeystoreUtils.load(KeystoreType.PKCS12, ksPath, "newkeystore");
 				assertTrue(ks.containsAlias("newentry"));
 				assertTrue(ks.isKeyEntry("newentry"));
-				PrivateKeyEntry copyKP = (PrivateKeyEntry) ks.getEntry("newentry", 
+				PrivateKeyEntry copyKP = (PrivateKeyEntry) ks.getEntry("newentry",
 						new KeyStore.PasswordProtection("newentry".toCharArray()));
 				assertArrayEquals(kp.getCertificate().getEncoded(), copyKP.getCertificate().getEncoded());
 			} catch (Throwable t) {
@@ -295,13 +324,13 @@ class KeystoreUtilsTest {
 			} catch (IOException e) {
 				System.err.println("Could not delete temp test file " + ksPath.toString());
 			}
-		}		
+		}
 	}
-	
+
 	@Test
 	void testLoadJCEKS() {
 		KeyStore ks = null;
-		
+
 		try {
 			ks = KeystoreUtils.load(TestUtils.getTestResource("sharedkey.jceks"), "sharedkey");
 		} catch (Throwable t) {
@@ -320,5 +349,5 @@ class KeystoreUtilsTest {
 		}
 		assertEquals(1, n);
 		assertEquals(1, pk);
-	}	
+	}
 }
